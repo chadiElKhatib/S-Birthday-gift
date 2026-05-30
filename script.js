53db6a3fc0dbd624ffbc00aec84b76145318c5d1
chadiElKhatib <chadi.kasswani51@gmail.com>
2026-05-30 05:26:13 +0200

    Destop
    Glemte og fjerne kommentar

--- script.js
+++ script.js
@@ -472,6 +472,6 @@ window.onload = () => {
   document.querySelector(".hero").style.display = "none";
   document.querySelector(".slideshow-section").classList.add("show");
 
-  currentSlide = 27;
+  currentSlide = 0;
   showSlide(currentSlide);  
   };*/
